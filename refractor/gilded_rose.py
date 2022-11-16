fc8c995d88f1ecfe9059d93dabf1abba04195ddb
Rodi <scheele@datlinq.com>
2022-11-16 15:50:57 +0100

    more customization options

--- refractor/gilded_rose.py
+++ refractor/gilded_rose.py
@@ -8,20 +8,24 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
 
+            base_item = BaseItem(item.name, item.sell_in, item.quality)
+
             match item.name:
                 case "Aged Brie":
-                    item = AgedBrie(item)
+                    child_item = AgedBrie(base_item.name, base_item.sell_in, base_item.quality)
                 case "Sulfuras, Hand of Ragnaros":
-                    item = Sulfuras(item)
+                    child_item = Sulfuras(base_item.name, base_item.sell_in, base_item.quality)
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    item = BackStagePass(item)
+                    child_item = BackStagePass(base_item.name, base_item.sell_in, base_item.quality)
                 case "Conjured Mana Cake":
-                    item = Conjured(item)
+                    child_item = Conjured(base_item.name, base_item.sell_in, base_item.quality)
                 case _:
-                    item = BaseItem(item)
+                    child_item = base_item
 
-            item.daily_sell_in()
-            item.daily_quality()
+            child_item.daily_routine()
+
+            item.quality = child_item.quality
+            item.sell_in = child_item.sell_in
 
 
 class Item:
@@ -35,62 +39,71 @@ class Item:
 
 
 class BaseItem(Item):
-    def __init__(self, item):
-        self.item = item
+    def __init__(self, name, sell_in, quality, default_quality_increment=1, max_quality=50, min_quality=0,
+                 legendary_quality=80):
+        super().__init__(name, sell_in, quality)
+        self.max_quality = max_quality
+        self.min_quality = min_quality
+        self.legendary_quality = legendary_quality
+        self.default_quality_increment = default_quality_increment
+
+    def daily_routine(self):
+        self.daily_sell_in()
+        self.daily_quality()
 
     def daily_quality(self):
-        if self.item.quality > 0:
-            if self.item.sell_in <= 0:
-                self.item.quality -= 2
+        if self.quality > self.min_quality:
+            if self.sell_in <= 0:
+                self.quality -= self.default_quality_increment * 2
             else:
-                self.item.quality -= 1
+                self.quality -= self.default_quality_increment
 
     def daily_sell_in(self):
-        self.item.sell_in -= 1
+        self.sell_in -= 1
 
 
 class AgedBrie(BaseItem):
-    def __init__(self, item):
-        self.item = item
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
     def daily_quality(self):
-        if self.item.quality < 50:
-            self.item.quality += 1
+        if self.quality < self.max_quality:
+            self.quality += self.default_quality_increment
 
 
 class Sulfuras(BaseItem):
-    def __init__(self, item):
-        self.item = item
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
     def daily_quality(self):
-        self.item.quality = 80
+        self.quality = self.legendary_quality
 
     def daily_sell_in(self):
-        self.item.sell_in = self.item.sell_in
+        self.sell_in = self.sell_in
 
 
 class BackStagePass(BaseItem):
-    def __init__(self, item):
-        self.item = item
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
     def daily_quality(self):
-        match self.item.sell_in:
+        match self.sell_in:
             case i if i == 0:
-                self.item.quality = 0
+                self.quality = 0
             case i if 1 <= i <= 5:
-                self.item.quality += 3
+                self.quality += 3
             case i if 6 <= i <= 10:
-                self.item.quality += 2
+                self.quality += 2
             case _:
-                self.item.quality += 1
+                self.quality += 1
 
-        if self.item.quality > 50:
-            self.item.quality = 50
+        if self.quality > self.max_quality:
+            self.quality = self.max_quality
 
 
 class Conjured(BaseItem):
-    def __init__(self, item):
-        self.item = item
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
 
     def daily_quality(self):
-        self.item.quality -= 2
+        self.quality -= self.default_quality_increment * 2
